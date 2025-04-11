cc8d9e8035039ce921719885330bf3d762583502
rebecaverasa <rebecaverasa@gmail.com>
2025-04-10 23:14:49 -0300

    wip header

---
+++ components/MyHeader.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const MyHeader = () => {
+    return (
+        <View>
+            <Text>aaaaaa</Text>
+        </View>
+    )
+}
+
+export default MyHeader
