85da7f2c4f314f2aab90422dedf328d0402ca65e
rebecaverasa <rebecaverasa@gmail.com>
2025-04-19 02:21:22 -0300

    profile icon

--- components/IconAddBook.jsx
+++ components/IconAddBook.jsx
@@ -2,12 +2,14 @@ import { Image, TouchableOpacity } from 'react-native';
 import addbook from "../assets/images/addbook.png";
 
 const IconAddBook = () => {
-    <TouchableOpacity onPress={() => console.log('Logo clicada')}>
-        <Image
-            source={addbook}
-            style={{ width: 120, height: 40, resizeMode: 'contain' }}
-        />
-    </TouchableOpacity>
-}
+    return (
+        <TouchableOpacity onPress={() => console.log('Logo clicada')}>
+            <Image
+                source={addbook}
+                style={{ width: 120, height: 40, resizeMode: 'contain' }}
+            />
+        </TouchableOpacity>
+    );
+};
 
 export default IconAddBook;
--- components/MyHeader.jsx
+++ components/MyHeader.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import { Header } from '@rneui/themed';
 import UserAvatar from './UserAvatar';
-import AddBook from './AddBook';
-import { Image, TouchableOpacity } from 'react-native';
-import addbook from "../assets/images/addbook.png";
+import IconAddBook from './IconAddBook';
+import IconProfile from './IconProfile';
 const MyHeader = () => {
     return (
         <Header
             backgroundColor="#2B2A2C"
             barStyle="default"
             centerContainerStyle={{}}
-            centerComponent={() => (
-                <TouchableOpacity onPress={() => console.log('Logo clicada')}>
-                    <Image
-                        source={addbook}
-                        style={{ width: 120, height: 40, resizeMode: 'contain' }}
-                    />
-                </TouchableOpacity>
-            )}
-            leftComponent={{ icon: "menu", color: "#F4D07A" }
-            }
-            leftContainerStyle={{}}
+            centerComponent={<IconAddBook />}
+            leftComponent={<IconProfile />}
             linearGradientProps={{}}
             placement="center"
             rightComponent={< UserAvatar />}
